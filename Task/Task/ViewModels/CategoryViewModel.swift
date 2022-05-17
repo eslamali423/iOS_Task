47e91075798754c21f63d9253f1d2237b2d41e8b
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-05-17 14:13:37 +0200

    Create CategoryViewModel

---
+++ Task/Task/ViewModels/CategoryViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  CategoryViewModel.swift
+//  Task
+//
+//  Created by Eslam Ali  on 16/05/2022.
+//
+
+import Foundation
+class CategoryViewModel {
+    
+    
+    var catgory : [Block] = []
+    var imageUrl = ""
+
+ //MARK:- Get All Categories
+    func getCat(completion : @escaping (Bool)->Void){
+        BaseDataAPI.shared.getData { (result) in
+            switch result {
+            case .success(let response):
+                self.imageUrl = response?.data.banners[0].content.backgroundImage ?? ""
+                for category in response?.data.banners[0].content.blocks ?? [] {
+                    self.catgory.append(category)
+                }
+                completion(true)
+              
+            case .failure(let error):
+                print(error.localizedDescription)
+                completion(false)
+            }
+        }
+    }
+    
+    
+}
+
