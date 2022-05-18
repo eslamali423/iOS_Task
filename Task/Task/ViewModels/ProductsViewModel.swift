ac1727d586eede64c8ad71d5a6bd880a3a778ecf
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-05-18 23:33:49 +0200

    Adding RxSwift and get data in BehaviorSubject

--- Task/Task/ViewModels/ProductsViewModel.swift
+++ Task/Task/ViewModels/ProductsViewModel.swift
@@ -6,15 +6,17 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
+
 class ProductsViewModel {
     
     
    
-//    var products = [[Any]]()
+
     var numberOfBanners = 0
-    var sectionBanner : [String] = []
     var content : [Content] = []
-    
+    var contents = BehaviorSubject(value: [Content]())
 
  //MARK:- Get All Products
  
@@ -23,20 +25,15 @@ class ProductsViewModel {
             switch restlt {
             case .success(let response):
                 for i in response?.data.banners ?? [] {
-        
+             
                     self?.content.append(i.content)
-                    
-                    
-//
-//                    for j in i.content.products ?? [] {
-//                        self?.products.append([i,j])
-//                    }
+                 
                     self?.numberOfBanners += 1
+                    
                 }
+                self?.contents.on(.next(self?.content ?? []))
             
-              //  print(self?.products[0][1])
-              //  print("SEEDCCTIOOONSSS")
-            //    print(self?.sectionBanner.count)
+               
                 completion(true)
                 
             case .failure(let error):
